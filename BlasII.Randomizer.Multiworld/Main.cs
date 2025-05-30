4df93b1bc0ac37a0fc7389fd95dc6859c67e789b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-05-30 17:46:52 -0400

    init: add base project files

---
+++ BlasII.Randomizer.Multiworld/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace BlasII.Randomizer.Multiworld;
+
+internal class Main : MelonMod
+{
+    public static Multiworld Multiworld { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        Multiworld = new Multiworld();
+    }
+}
---
+++ BlasII.Randomizer.Multiworld/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using BlasII.Randomizer.Multiworld;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(BlasII.Randomizer.Multiworld.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
