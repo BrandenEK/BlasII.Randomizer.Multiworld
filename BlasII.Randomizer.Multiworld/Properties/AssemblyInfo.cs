2377ef350597548cddd9d57d2fca7bc220952c89
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-05-30 17:58:15 -0400

    core: setup project files

--- BlasII.Randomizer.Multiworld/Properties/AssemblyInfo.cs
+++ BlasII.Randomizer.Multiworld/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.Randomizer.Multiworld;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.Randomizer.Multiworld.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
+[assembly: MelonOptionalDependencies("Modding API", "2.0.0", "Menu Framework", "0.3.0", "Randomizer", "2.3.0")]
