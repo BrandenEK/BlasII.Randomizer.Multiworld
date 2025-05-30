13b6c356f4b074422ea46de3e52265d6a55cbc07
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-05-30 18:01:01 -0400

    core: setup base project (#1)
    * core: setup project files
    
    * core: add workflow
    
    * core: add license
    
    * docs: add basic readme

--- BlasII.Randomizer.Multiworld/Multiworld.cs
+++ BlasII.Randomizer.Multiworld/Multiworld.cs
@@ -2,6 +2,9 @@
 
 namespace BlasII.Randomizer.Multiworld;
 
+/// <summary>
+/// A multiworld client that allows the Randomizer to connect to AP
+/// </summary>
 public class Multiworld : BlasIIMod
 {
     internal Multiworld() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# Blasphemous 2 Multiworld
+
+<img src="https://img.shields.io/github/downloads/BrandenEK/BlasII.Randomizer.Multiworld/total?color=872124&style=for-the-badge">
+
+---
+
+indev
