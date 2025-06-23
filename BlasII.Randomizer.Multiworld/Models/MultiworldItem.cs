c10dc1387520dfad107210b8b3402074acfd27ba
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-06-23 15:15:52 -0400

    core: override rando shuffle with fake one

---
+++ BlasII.Randomizer.Multiworld/Models/MultiworldItem.cs
@@ -0,0 +1,18 @@
+﻿using BlasII.Randomizer.Models;
+
+namespace BlasII.Randomizer.Multiworld.Models;
+
+public class MultiworldItem : Item
+{
+    public int Player { get; set; }
+
+    public MultiworldItem(bool progression, int player)
+    {
+        Id = "internal location id";
+        Name = "name of item";
+        Type = ItemType.Invalid;
+        Progression = progression; // Replaced with 'class'
+        Count = 0;
+        Player = player;
+    }
+}
---
+++ BlasII.Randomizer.Multiworld/Patches/RandomizerPatches.cs
@@ -0,0 +1,42 @@
+﻿using BlasII.ModdingAPI;
+using BlasII.Randomizer.Handlers;
+using BlasII.Randomizer.Models;
+using BlasII.Randomizer.Multiworld.Models;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace BlasII.Randomizer.Multiworld.Patches;
+
+/// <summary>
+/// Finds items from an internal storage instead of the Randomizer's
+/// </summary>
+[HarmonyPatch(typeof(ItemHandler), nameof(ItemHandler.GetItemAtLocation))]
+class ItemHandler_GetItemAtLocation_Patch
+{
+    public static bool Prefix(string locationId, ref Item __result)
+    {
+        __result = new MultiworldItem(false, 1);
+        return false;
+    }
+}
+
+/// <summary>
+/// Overrides the Randomizer's shuffle with a fake one
+/// </summary>
+[HarmonyPatch(typeof(ItemHandler), nameof(ItemHandler.ShuffleItems))]
+class ItemHandler_ShuffleItems_Pstch
+{
+    public static bool Prefix(ItemHandler __instance)
+    {
+        ModLog.Info("Overriding ItemHandler shuffle");
+
+        var mapping = new Dictionary<string, string>();
+        foreach (var location in Main.Randomizer.ItemLocationStorage.AsSequence)
+        {
+            mapping.Add(location.Id, location.Id);
+        }
+
+        __instance.MappedItems = mapping;
+        return false;
+    }
+}
