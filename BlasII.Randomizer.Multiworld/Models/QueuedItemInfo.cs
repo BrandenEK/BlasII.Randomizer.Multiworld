bf7fb0c7a377877a526150dc9c2ffc3d22885ad4
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-06-23 02:08:55 -0400

    core: fix QueuedItemInfo typo

--- BlasII.Randomizer.Multiworld/Models/QueuedItemInfo.cs
+++ BlasII.Randomizer.Multiworld/Models/QueuedItemInfo.cs
@@ -5,12 +5,12 @@ namespace BlasII.Randomizer.Multiworld.Models;
 /// <summary>
 /// Data for an item received from the AP server in the queue
 /// </summary>
-public class QueuedItemInfo(ItemInfo info, int index)
+public class QueuedItemInfo(ItemInfo item, int index)
 {
     /// <summary>
     /// Data for the actual received item
     /// </summary>
-    public ItemInfo Item { get; } = info;
+    public ItemInfo Item { get; } = item;
 
     /// <summary>
     /// THe received item index
