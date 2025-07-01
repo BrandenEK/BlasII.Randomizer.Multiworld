7bd89a76c40a4a89e4ff43cd041ed7a8e92c1a18
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-01 00:55:22 -0400

    core: add display patch

---
+++ BlasII.Randomizer.Multiworld/Displays/StatusDisplay.cs
@@ -0,0 +1,27 @@
+﻿using BlasII.Randomizer.Multiworld.Models;
+
+namespace BlasII.Randomizer.Multiworld.Displays;
+
+/// <summary>
+/// Displays the connection status on the UI
+/// </summary>
+public class StatusDisplay
+{
+    private readonly ServerConnection _connection;
+
+    /// <summary>
+    /// Initializes the status connection
+    /// </summary>
+    public StatusDisplay(ServerConnection connection)
+    {
+        _connection = connection;
+    }
+
+    /// <summary>
+    /// Updates the connection icon
+    /// </summary>
+    public void OnUpdate()
+    {
+
+    }
+}
