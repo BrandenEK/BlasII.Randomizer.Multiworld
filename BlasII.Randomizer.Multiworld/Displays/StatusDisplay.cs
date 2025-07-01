68143962450d96346cf79dede18d7db253c436b9
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-01 01:14:38 -0400

    core: move image creation to Display

--- BlasII.Randomizer.Multiworld/Displays/StatusDisplay.cs
+++ BlasII.Randomizer.Multiworld/Displays/StatusDisplay.cs
@@ -1,4 +1,10 @@
-﻿using BlasII.Randomizer.Multiworld.Models;
+﻿using BlasII.Framework.UI;
+using BlasII.ModdingAPI;
+using BlasII.ModdingAPI.Helpers;
+using BlasII.Randomizer.Multiworld.Models;
+using Il2Cpp;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace BlasII.Randomizer.Multiworld.Displays;
 
@@ -8,6 +14,8 @@ namespace BlasII.Randomizer.Multiworld.Displays;
 public class StatusDisplay
 {
     private readonly ServerConnection _connection;
+
+    private Image _image;
 
     /// <summary>
     /// Initializes the status connection
@@ -22,6 +30,34 @@ public class StatusDisplay
     /// </summary>
     public void OnUpdate()
     {
+        if (!SceneHelper.GameSceneLoaded)
+            return;
 
+        if (_image == null)
+            _image = CreateImage(Object.FindObjectOfType<UITearsControl>().transform);
+
+        if (_image == null)
+        {
+            ModLog.Error("Failed to create the connection status image");
+            return;
+        }
+
+        _image.sprite = Main.Multiworld.IconStorage.GetConnectionIcon(_connection.Connected);
+    }
+
+    private Image CreateImage(Transform parent)
+    {
+        RectTransform connect = UIModder.Create(new RectCreationOptions()
+        {
+            Name = "Connection",
+            Parent = parent,
+            Pivot = Vector2.one,
+            XRange = Vector2.one,
+            YRange = Vector2.zero,
+            Position = new Vector2(0, 0),
+            Size = new Vector2(64, 64),
+        });
+
+        return connect.AddImage();
     }
 }
