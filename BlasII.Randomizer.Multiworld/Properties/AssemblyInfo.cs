44c615ff7eaaec58435e7356c697784228d52356
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-21 08:56:57 -0400

    core: upgrade rando version

--- BlasII.Randomizer.Multiworld/Properties/AssemblyInfo.cs
+++ BlasII.Randomizer.Multiworld/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using BlasII.Randomizer.Multiworld;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.Randomizer.Multiworld.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "2.0.0", "UI Framework", "0.1.0", "Menu Framework", "0.3.0", "Randomizer", "2.3.0")]
+[assembly: MelonOptionalDependencies("Modding API", "2.0.0", "UI Framework", "0.1.0", "Menu Framework", "0.3.0", "Randomizer", "2.4.0")]
 [assembly: MelonPriority(10)]
